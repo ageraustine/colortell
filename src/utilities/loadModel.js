65384daff9fbb02df92f7a75e52ff3610f4c8ab4
ageraustine <agerausten@gmail.com>
2022-10-20 23:14:36 +0300

    preprocess

--- src/utilities/loadModel.js
+++ src/utilities/loadModel.js
@@ -1,8 +1,8 @@
 const tf = require('@tensorflow/tfjs');
 const tfnode = require('@tensorflow/tfjs-node');
 
-async function loadModel(){
-    const handler = tfnode.io.fileSystem('tfjs_model/model.json');
+async function loadModel(modelPath){
+    const handler = tfnode.io.fileSystem(modelPath);
     const model = await tf.loadLayersModel(handler);
     return model
 }
--- src/utilities/preprocess.js
+++ src/utilities/preprocess.js
@@ -3,7 +3,7 @@ const tf = require('@tensorflow/tfjs');
 async function preprocess(image){
     scalingLayer = tf.layers.rescaling(1/255)
     const img = scalingLayer(img)
-    img = tf.expandDims(image,0)
+    img = tf.expandDims(img,0)
     return tf.image.resizeBilinear(img,[256, 256])
 }
 
